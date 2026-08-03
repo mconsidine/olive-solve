7d50bb02158de70ceea2982b33cb63e983399133
Omair Kamil <oakamil@gmail.com>
2026-08-03 20:44:12 +0100

    Add support for MPU series IMUs.

--- olive-imu/src/lib.rs
+++ olive-imu/src/lib.rs
@@ -9,6 +9,8 @@ pub mod bmi160;
 pub mod bno055;
 /// Driver for the BNO085 IMU.
 pub mod bno085;
+/// Driver for the MPU Series IMUs (MPU6000, MPU6050, MPU6500, MPU9150, MPU9250).
+pub mod mpuxxxx;
 /// Core IMU trait, states, and data structures.
 pub mod imu;
 /// Storage trait for persisting IMU calibration data.
@@ -17,5 +19,6 @@ pub mod storage;
 pub use bmi160::*;
 pub use bno055::*;
 pub use bno085::*;
+pub use mpuxxxx::*;
 pub use imu::*;
 pub use storage::*;
